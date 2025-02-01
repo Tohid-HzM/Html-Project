8fd1f4c85fcf4c044fe48ba02e039551e6a6a5a4
Tohid_HzM <tohidhzm@gamil.com>
2025-02-01 19:31:08 +0330

    collapsible

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(elems);
+  });
+
+  
